cf73794d20ab7f99eab58317eeb9593432a4de78
Chris Dutton <chris@aquatic-edge.com>
2014-09-08 23:42:15 -0700

    Update stripe initializer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,7 +3,7 @@ if Rails.env.development?
   Stripe.api_key = "sk_test_N9jcRhTJ0pQB78HtSOpAs3Jf"
   STRIPE_PUBLIC_KEY = "pk_test_8cuWJKwYw2wM893BTHEPP4Ty"
 end
-if Rails.env.production?
+if Rails.env.production? or Rails.env.staging?
   Stripe.api_key = "sk_live_UyCk8CQd1HvLZjzO0Cs06GEX"
   STRIPE_PUBLIC_KEY = "pk_live_GpRpRJm5lLT9gh9GIvNKW6Cr"
 end
