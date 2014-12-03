90eba4f5855e4325851ffb0f12fbc3accb0b07b6
Chris Dutton <chris@rosepointnav.com>
2014-12-03 15:45:53 -0800

    Updated email server to boomerangproof.com account

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.gmail.com',
+    :address => 'smtpout.secureserver.net',
     :port => 587,
-    :domain => 'aquatic-edge.com',
+    :domain => 'boomerangproof.com',
     :authentication => 'plain',
-    :user_name => 'chris@aquatic-edge.com',
+    :user_name => 'chris@boomerangproof.com',
     :password => '05test05',
     :enable_starttls_auto => true
 }
